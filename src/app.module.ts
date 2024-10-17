a367733a7e85f00771647cc07fecaccb7756cfbf
Daniel Zamignani <danielzamignani@gmail.com>
2024-10-17 18:09:31 -0300

    chore(linters): update prettier and eslint config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { typeORMConnection } from './database/typeorm.connection';
 
 @Module({
-  imports: [typeORMConnection],
-  controllers: [],
-  providers: [],
+    imports: [typeORMConnection],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -1,19 +1,18 @@
 import { config as dotenvConfig } from 'dotenv';
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 
 dotenvConfig({ path: '.env' });
 
-export const typeormConfig: any = {
-  type: 'postgres',
-  host: `${process.env.DATABASE_HOST}`,
-  port: +process.env.DATABASE_PORT,
-  username: `${process.env.DATABASE_USERNAME}`,
-  password: `${process.env.DATABASE_PASSWORD}`,
-  database: `${process.env.DATABASE_NAME}`,
-  entities: [__dirname + '/entities/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/migrations/*{.ts,.js}'],
-  autoLoadEntities: true,
-  synchronize: false,
+export const typeormConfig: DataSourceOptions = {
+    type: 'postgres',
+    host: `${process.env.DATABASE_HOST}`,
+    port: +process.env.DATABASE_PORT,
+    username: `${process.env.DATABASE_USERNAME}`,
+    password: `${process.env.DATABASE_PASSWORD}`,
+    database: `${process.env.DATABASE_NAME}`,
+    entities: [__dirname + '/entities/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/migrations/*{.ts,.js}'],
+    synchronize: false,
 };
 
 export default new DataSource(typeormConfig);
