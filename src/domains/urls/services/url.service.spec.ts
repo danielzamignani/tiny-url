5d8b7a52d68ab4a14157c1c6e58724972061822b
Daniel Zamignani <danielzamignani@gmail.com>
2024-10-19 19:26:11 -0300

    test(urls service): add tests for get users urls method

--- src/domains/urls/services/url.service.spec.ts
+++ src/domains/urls/services/url.service.spec.ts
@@ -6,14 +6,46 @@ import { Url } from '../../../database/entities/urls.entity';
 import { Repository } from 'typeorm';
 import { urlMock, urlRepositoryMock } from '../mocks/url.repository.mock';
 import { CreateShortUrlResponseDTO } from '../dtos/create-short-url.res.dto';
+import { PaginationRequestDTO } from '../../../shared/dtos/pagination.req.dto';
+import { VwActiveUrl } from '../../../database/entities/vw-active-urls.entity';
+import { paginate } from 'nestjs-typeorm-paginate';
 
 jest.mock('nanoid', () => ({
     nanoid: jest.fn(),
 }));
 
+jest.mock('nestjs-typeorm-paginate', () => ({
+    paginate: jest.fn().mockResolvedValue({
+        items: [
+            {
+                id: '5c93f941-8a75-4ab1-bbe7-15a21fa17df1',
+                original_url: 'https://www.google.com.br',
+                short_url: 'http://localhost:3000/9KE2cJ',
+                created_at: '2024-10-19T21:34:38.525Z',
+                updated_at: '2024-10-19T21:34:38.525Z',
+            },
+        ],
+        meta: {
+            currentPage: 1,
+            itemCount: 10,
+            itemsPerPage: 10,
+            totalItems: 10,
+            totalPages: 1,
+        },
+    }),
+}));
+
+const userId = '12345';
+
+const queryBuilderMock = {
+    where: jest.fn(),
+    orderBy: jest.fn(),
+};
+
 describe('UrlService Tests', () => {
     let urlService: UrlService;
     let urlRepository: Repository<Url>;
+    let vwActiveUrlRepository: Repository<VwActiveUrl>;
 
     beforeAll(async () => {
         const urlModule: TestingModule = await Test.createTestingModule({
@@ -23,10 +55,21 @@ describe('UrlService Tests', () => {
                     provide: getRepositoryToken(Url),
                     useValue: urlRepositoryMock,
                 },
+                {
+                    provide: getRepositoryToken(VwActiveUrl),
+                    useValue: {
+                        createQueryBuilder: jest
+                            .fn()
+                            .mockImplementation(() => queryBuilderMock),
+                    },
+                },
             ],
         }).compile();
         urlService = urlModule.get<UrlService>(UrlService);
         urlRepository = urlModule.get<Repository<Url>>(getRepositoryToken(Url));
+        vwActiveUrlRepository = urlModule.get<Repository<VwActiveUrl>>(
+            getRepositoryToken(VwActiveUrl)
+        );
     });
 
     afterEach(() => {
@@ -41,7 +84,6 @@ describe('UrlService Tests', () => {
         const url = 'https://www.google.com';
         const nanoIdMock = 'ABC123';
         const urlBase = 'https://localhost:3000';
-        const userId = '1234';
 
         beforeAll(() => {
             (nanoid as jest.Mock).mockReturnValue(nanoIdMock);
@@ -91,6 +133,47 @@ describe('UrlService Tests', () => {
                 originalUrl: url,
                 shortUrl: `undefined/${nanoIdMock}`, //Testar com a env
             } as CreateShortUrlResponseDTO);
+        });
+    });
+
+    describe('getUserUrls', () => {
+        const paginationOptions: PaginationRequestDTO = {
+            page: '1',
+            limit: '10',
+        };
+
+        it('should create a querybuilder', async () => {
+            await urlService.getUserUrls(userId, paginationOptions);
+
+            expect(
+                vwActiveUrlRepository.createQueryBuilder
+            ).toHaveBeenCalledTimes(1);
+        });
+
+        it('should filter by the user id', async () => {
+            await urlService.getUserUrls(userId, paginationOptions);
+
+            expect(queryBuilderMock.where).toHaveBeenCalledTimes(1);
+            expect(queryBuilderMock.where).toHaveBeenCalledWith(
+                'user_id = :userId',
+                { userId }
+            );
+        });
+
+        it('should order by last update date', async () => {
+            await urlService.getUserUrls(userId, paginationOptions);
+
+            expect(queryBuilderMock.orderBy).toHaveBeenCalledTimes(1);
+            expect(queryBuilderMock.orderBy).toHaveBeenCalledWith(
+                'updated_at',
+                'DESC'
+            );
+        });
+
+        it('should call paginate method', async () => {
+            await urlService.getUserUrls(userId, paginationOptions);
+
+            expect(paginate).toHaveBeenCalledTimes(1);
         });
     });
 });
