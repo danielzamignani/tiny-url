d23a2b2262191a4eac752d91c3b3332d30d4a406
Daniel Zamignani <danielzamignani@gmail.com>
2024-10-19 19:24:07 -0300

    feat(urls view): add active urls view

---
+++ src/database/migrations/1729369417416-CreateVwValidUrls.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateVwActiveUrls1729369417416 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE VIEW 
+                "vw_active_urls" 
+            AS
+                SELECT 
+                    u.id,
+                    u.original_url,
+                    u.short_url,
+                    u.created_at,
+                    u.updated_at,
+                    u.user_id
+                FROM 
+                    urls u
+                WHERE
+                    u.deleted_at IS NULL
+            ;  
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropView('vw_active_urls');
+    }
+}
