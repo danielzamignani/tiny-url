9aa85338762c6a8946f73f2f0991bdbe445bbd18
Daniel Zamignani <danielzamignani@gmail.com>
2024-10-19 17:11:38 -0300

    feat(jwt guard): add jwt simple guard

--- src/shared/decorators/current-user.decorator.ts
+++ src/shared/decorators/current-user.decorator.ts
@@ -1,14 +1,15 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 import { jwtDecode } from 'jwt-decode';
 import { ICurrentUser } from '../interfaces/current-user.interface';
+import { extractTokenFromHeader } from '../helpers/auth.helper';
 
 export const CurrentUser = createParamDecorator(
     (data: unknown, context: ExecutionContext): Promise<ICurrentUser> => {
         const request = context.switchToHttp().getRequest();
-        const jwt = request.headers['authorization'];
+        const token = extractTokenFromHeader(request);
 
-        if (jwt) {
-            return jwtDecode(jwt);
+        if (token) {
+            return jwtDecode(token);
         } else {
             return null;
         }
---
+++ src/shared/guards/simple-jwt.guard.ts
@@ -0,0 +1,35 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { extractTokenFromHeader } from '../helpers/auth.helper';
+
+@Injectable()
+export class SimpleJwtGuard implements CanActivate {
+    constructor(private jwtService: JwtService) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const token = extractTokenFromHeader(request);
+
+        if (!token) {
+            throw new UnauthorizedException(
+                'You must be logged in to access this resource'
+            );
+        }
+
+        try {
+            await this.jwtService.verifyAsync(token, {
+                secret: process.env.JWT_SECRET,
+            });
+        } catch {
+            throw new UnauthorizedException('Invalid or expired token');
+        }
+
+        return true;
+    }
+}
