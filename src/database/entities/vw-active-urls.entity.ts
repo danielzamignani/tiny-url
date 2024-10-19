09cfe7f6ddb1cbc1a2c56002ceda0b5c3863e93d
Daniel Zamignani <danielzamignani@gmail.com>
2024-10-19 19:24:29 -0300

    feat(urls entity): add active urls entity

---
+++ src/database/entities/vw-active-urls.entity.ts
@@ -0,0 +1,19 @@
+import { ViewColumn, ViewEntity } from 'typeorm';
+
+@ViewEntity('vw_active_urls')
+export class VwActiveUrl {
+    @ViewColumn()
+    id: string;
+
+    @ViewColumn()
+    original_url: string;
+
+    @ViewColumn()
+    short_url: string;
+
+    @ViewColumn()
+    created_at: string;
+
+    @ViewColumn()
+    updated_at: string;
+}
