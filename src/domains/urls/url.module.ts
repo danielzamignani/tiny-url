b0ea7d68077d80ed9e1ee520278a583049a082d9
Daniel Zamignani <danielzamignani@gmail.com>
2024-10-19 19:27:31 -0300

    fix(urls module): add view in typeorm module

--- src/domains/urls/url.module.ts
+++ src/domains/urls/url.module.ts
@@ -3,9 +3,10 @@ import { Url } from 'src/database/entities/urls.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UrlController } from './controllers/url.controller';
 import { UrlService } from './services/url.service';
+import { VwActiveUrl } from 'src/database/entities/vw-active-urls.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Url])],
+    imports: [TypeOrmModule.forFeature([Url, VwActiveUrl])],
     controllers: [UrlController],
     providers: [UrlService],
 })
